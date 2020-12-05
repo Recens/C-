e7f81348efd9e1b3b820cd17cc134c8762809a08
Recens <d96dee22>
2020-12-05 19:17:24 +0200

    lb_2

---
+++ lb_2/Form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lb_2
+{
+    public partial class Form2 : Form
+    {
+        public Form2(Form1 f_1)
+        {
+            InitializeComponent();
+            f_1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3 f3 = new Form3(this);
+            f3.ShowDialog();
+        }
+    }
+}
---
+++ lb_2/Form3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lb_2
+{
+    public partial class Form3 : Form
+    {
+        public Form3(Form2 form2)
+        {
+            InitializeComponent();
+            form2.Visible = false;
+        }
+
+        private void Form3_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+        
+    }
+}

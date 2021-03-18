2a9b4e156a9333785b30204249c759d138913f64
Recens <d96dee22>
2021-03-18 22:24:47 +0200

    Lab_2 LMV

---
+++ Lab_2/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab_2
+{
+    public partial class Form2 : Form
+    {
+        public Form2(Form1 f_1)
+        {
+            InitializeComponent();
+            f_1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3 f3 = new Form3(this);
+            f3.ShowDialog();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 f3 = new Form3(this);
+            f3.ShowDialog();
+            this.Hide();
+        }
+    }
+}
---
+++ Lab_2/Form3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab_2
+{
+    public partial class Form3 : Form
+    {
+        public Form3(Form2 f_2)
+        {
+            InitializeComponent();
+            f_2.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
